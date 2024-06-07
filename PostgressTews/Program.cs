fc61ce942146bdb0d03712d34df0aed3ea9ad182
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2024-06-07 03:26:55 +0300

    Добавьте файлы проекта.

---
+++ PostgressTews/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace PostgressTews
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<Context>(options =>
+            {
+                options.UseNpgsql("Host=82.97.248.8;Port=5432;Database=default_db;Username=gen_user;Password=:z)gb?12c.UWyi");
+            });
+            var app = builder.Build();
+
+            app.MapGet("/", (Context context) =>
+            {
+                return context.Users.FirstOrDefault(); 
+            });
+
+            app.Run();
+        }
+    }
+}
---
+++ PostgressTews/Users.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostgressTews
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
