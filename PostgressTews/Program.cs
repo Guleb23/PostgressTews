f896cf19593e76bf254c2b01003e89d5d6dce71e
Guleb23 <112973753+Guleb23@users.noreply.github.com>
2024-06-07 17:08:33 +0300

    i76t

--- PostgressTews/Program.cs
+++ PostgressTews/Program.cs
@@ -7,15 +7,14 @@ namespace PostgressTews
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+            builder.Services.AddEndpointsApiExplorer();
 
             builder.Services.AddDbContext<Context>(options =>
             {
                 options.UseNpgsql("Host=82.97.248.8;Port=5432;Database=default_db;Username=gen_user;Password=:z)gb?12c.UWyi");
             });
             var app = builder.Build();
-            app.Urls.Add("http://localhost:5000");
-
+            app.UseHttpsRedirection();
             app.MapGet("/", (Context context) =>
             {
                 return context.Users.FirstOrDefault(); 
